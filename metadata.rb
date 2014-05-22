5f56462e318f40c0bec67f84424b15a846100bb8
Eric Herot <eric.github@herot.com>
2014-05-22 10:38:28 -0400

    First rev

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'et_storm'
+maintainer       'EverTrue, Inc.'
+maintainer_email 'eric.herot@evertrue.com'
+license          'All rights reserved'
+description      'Installs/Configures et_storm'
+long_description 'Installs/Configures et_storm'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: et_storm
+# Recipe:: default
+#
+# Copyright (C) 2014 EverTrue, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
