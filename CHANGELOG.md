835f9a1e9cb277c274b6a742a5ac38dbb7d519af
Jeff Byrnes <jeff@evertrue.com>
2014-11-14 17:53:12 -0500

    Bump to v1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v1.1.0 (2014-11-14)
+
+* Add sudoer rules for topology deployments
+
 ## v1.0.1 (2014-09-23)
 
 * Bump worker slots
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'eric.herot@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_storm'
 long_description 'Installs/Configures et_storm'
-version          '1.0.1'
+version          '1.1.0'
 
 depends 'apache_storm', '= 3.0.0'
 depends 'et_swap', '= 1.0.1'
