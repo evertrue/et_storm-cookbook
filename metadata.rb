bd3b6b5433cfaec065fd82b7ee8f0f836d0fdde6
Eric Herot <eric.github@herot.com>
2015-03-02 16:00:34 -0500

    Bump apache_storm to 4.0.1

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures et_storm'
 long_description 'Installs/Configures et_storm'
 version          '2.0.1'
 
-depends 'apache_storm', '= 4.0.0'
+depends 'apache_storm', '= 4.0.1'
 depends 'et_swap', '~> 1.0'
 depends 'sudo',  '~> 2.7'
