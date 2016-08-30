446bba84d99e78017fe911de9acf200e0fa11c0d
Hai Zhou <islandsword@gmail.com>
2016-08-30 16:20:01 -0400

    Remove old java conf

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,8 +7,4 @@
 # All rights reserved - Do Not Redistribute
 #
 
-node.set['java']['jdk_version'] = '7'
-node.set['java']['java_home'] = '/usr/lib/jvm/java-7-openjdk-amd64'
-node.set['java']['java_exec'] = node['java']['java_home'] + '/bin/java'
-
 node.set['storm']['yaml']['ui.childopts'] = '-Xmx256m'
