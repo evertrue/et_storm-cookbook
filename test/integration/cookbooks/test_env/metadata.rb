e302360e16ef3d36ed7c821e2263a9346c04da8d
Eric Herot <eric.github@herot.com>
2016-10-07 17:38:37 -0400

    Add vault environment setup to the test harness

--- test/integration/cookbooks/test_env/metadata.rb
+++ test/integration/cookbooks/test_env/metadata.rb
@@ -1,2 +1,4 @@
 name 'test_env'
 version '1.0.1'
+
+depends 'magic'
--- test/integration/cookbooks/test_env/recipes/default.rb
+++ test/integration/cookbooks/test_env/recipes/default.rb
@@ -1,4 +1,17 @@
 package 'zookeeper'
 package 'zookeeperd'
 
+node.override['etc_environment']['VAULT_ADDR'] = 'http://vault.service.us-east-1.consul:8200'
+node.override['etc_environment']['VAULT_TOKEN'] = 'TEST_TOKEN'
+
+directory '/etc/profile.d'
+
+file '/etc/environment' do
+  content node['etc_environment'].map { |k, v| "#{k}=#{v}" }.join("\n")
+end
+
+file '/etc/profile.d/evertrue.sh' do
+  content lazy { (exports_config(node['etc_environment']) + "\n") }
+end
+
 cookbook_file '/etc/hosts'
